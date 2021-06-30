916558788d2542b28c64f338c41a73929e6fa933
Veronica Blake/Noyce <vrex149@gmail.com>
2021-06-30 15:15:07 -0600

    working on random

---
+++ app.js
@@ -0,0 +1,44 @@
+let choices = {
+    rock: {
+        beats: 'scissors',
+        loses: 'paper',
+        ties: 'rock',
+        btnClass: 'btn-dark'
+    },
+    paper: {
+        beats: 'rock',
+        loses: 'scissors',
+        ties: 'paper',
+        btnClass: 'btn-warning'
+    },
+    scissors: {
+        beats: 'paper',
+        loses: 'rock',
+        ties: 'scissors',
+        btnClass: 'btn-danger'
+    }
+}
+
+function drawButtons(){
+    let template = ''
+    for(let key in choices){
+        let choice = choices[key]
+        template += `<button class="btn ${choice.btnClass} mx-1" onclick="player('${key}')">${key}</button>`
+    }
+    document.getElementById('buttons').innerHTML = template
+}
+
+function player(choiceName){
+    let choice = choices[choiceName]
+    choiceName = choiceName[0].toUpperCase() + choiceName.substr(1)
+    console.log(choice)
+    document.getElementById("player-output").innerText = `You Chose: ${choiceName}`
+}
+
+function computer(){
+    let options = Object.keys(choices)
+    let randIndex = Math.floor(Math.random() * (choices.length))
+    console.log(options[randIndex])
+}
+
+drawButtons();
